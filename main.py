6f082c19859afb709d44bd3620e7a8b5f0dd2899
AidenTran15 <aidenkiettran@gmail.com>
2020-05-12 18:22:57 +1000

    test window display

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class Bird:
         self.vel = 0
         self.height = self.y
         self.img_count = 0
-        self.img = self.IMG[0]
+        self.img = self.IMGS[0]
 
     def jump(self):
         self.vel = -10.5
@@ -59,24 +59,48 @@ class Bird:
         self.img_count += 1
         
         if self.img_count < self.ANIMATION_TIME:
-            self.img = self.IMG[0]
+            self.img = self.IMGS[0]
         elif self.img_count < self.ANIMATION_TIME*2:
-            self,img = self.IMG[1]
+            self.img = self.IMGS[1]
         elif self.img_count < self.ANIMATION_TIME*3:
-            self.img = self.IMG[2]
+            self.img = self.IMGS[2]
         elif self.img_count < self.ANIMATION_TIME *4:
-            self.img = self.IMG[1]
+            self.img = self.IMGS[1]
         elif self.img_count < self.ANIMATION_TIME*4 +1:
-            self.img = self.IMG[0]
+            self.img = self.IMGS[0]
             self.img_count = 0
 
         if self.titl <= -80:
             self.img = self.IMG[1]
             self.img_count = self.ANIMATION_TIME*2
 
-        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        rotated_image = pygame.transform.rotate(self.img, self.titl)
         new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft = (self.x, self.y)).center)
         win.blit(rotated_image, new_rect.topleft)
 
     def get_mask(self):
-        
+        return pygame.from_surface(self.img)
+
+
+def draw_window(win, bird):
+    win.blit(BG_IMG, (0,0))
+    bird.draw(win)
+    pygame.display.update()
+
+
+def main():
+    bird = Bird(200,200)
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+        draw_window(win, bird)
+
+    pygame.quit()
+    quit()
+
+main()
